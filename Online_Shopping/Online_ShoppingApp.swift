e5ab16a9f7ed4dfa06ca46c105cf162d1a3181ab
dilki3 <dilkidinesha345@gmail.com>
2024-03-12 09:56:16 +0530

    Initial Commit

---
+++ Online_Shopping/Online_ShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_ShoppingApp.swift
+//  Online_Shopping
+//
+//  Created by Dila Dinesha on 2024-03-12.
+//
+
+import SwiftUI
+
+@main
+struct Online_ShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
