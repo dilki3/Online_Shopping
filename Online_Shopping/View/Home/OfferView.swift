f712718a2bf0fc1e2d27083e439efea2ef116076
dilki3 <dilkidinesha345@gmail.com>
2024-03-25 00:40:27 +0530

    Updated

---
+++ Online_Shopping/View/Home/OfferView.swift
@@ -0,0 +1,18 @@
+//
+//  OfferView.swift
+//  Online_Shopping
+//
+//  Created by Dila Dinesha on 2024-03-24.
+//
+
+import SwiftUI
+
+struct OfferView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OfferView()
+}
